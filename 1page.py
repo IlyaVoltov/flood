caa855bfe86457da5101425171efcc119f1a8eef
IlyaVoltov <39305458+IlyaVoltov@users.noreply.github.com>
2020-06-19 19:02:55 +0300

    Add files via upload

--- 1page.py
+++ 1page.py
@@ -30,80 +30,83 @@ def display_page(pathname):
 def get_menu():
 
     menu = html.Div([
-            dcc.Link('Норильская ТЭЦ-3   ', href='/norilsk', id='link-1', className='tab first'),
-            dcc.Link('Волховская ГЭС  ', href='/volhov', id='link-2', className='tab'),
-            dcc.Link('Севро-Западная ТЭЦ  ', href='/northwest2', id='link-3', className='tab'),
+            dcc.Link('Норильская ТЭЦ-3', href='/norilsk', id='link-1', className='tab first'),
+            dcc.Link('сайт РПН', href='https://rpn.gov.ru/', id='link-2', className='tab'),
         ], className='screen-top')
 
     return menu
 
-def get_buttons():
-
-    buttons = html.Div([
-
-        html.Div([
-            html.A([
-                html.Img(
-                    src=app.get_asset_url('norilsk.jpg'),
-                    className='btn-img'
-                ),
-                html.H3('Норильская ТЭЦ-3'),
-            ], href='/norilsk')
-        ], className='grid-item'),
-
-        html.Div([
-            html.A([
-                html.Img(
-                    src=app.get_asset_url('volhov.jpg'),
-                    className='btn-img'
-            ),
-            html.H3('Волховская ГЭС'),
-             ], href='/volhov')
-        ], className='grid-item'),
-
-        html.Div([
-                 html.A([
-                    html.Img(
-                    src=app.get_asset_url('northwest2.png'),
-                    className='btn-img'
-            ),
-            html.H3('Северо-Западная ТЭЦ'),
-            ], href='/northwest2')
-            ], className = 'grid-item'),
-
-    ], className='grid-container')
-
-    return buttons
-
 def generate_frontpage():
     frontpage = html.Div(id='las-header', children=[
+        html.A(
             html.Img(
                 id='las-logo',
                 src='data:image/jpg;base64,{}'.format(
                     base64.b64encode(
-                        open('assets/rosprirodnadzor.jpg', 'rb').read()
+                        open('assets/rosprirodnadzor.png', 'rb').read()
                     ).decode()
+                )), href='https://rpn.gov.ru/'),
+        html.Div(
+            id='las-header-text',
+            children=[
+                html.H1("Аварии")]
                 )
-            ),
-            html.Img(
-                id='logoftc',
-                src='data:image/jpg;base64,{}'.format(
-                    base64.b64encode(
-                    open('assets/whodeploy.jpg', 'rb').read()
-                ).decode()
-            )),
-            html.Div(
-                id='las-header-text',
-                children=[
-                    html.H1("Система мониторинга экологической обстановки в РФ")]
-            )
         ])
 
     return  frontpage
 
+def table_link():
+    table = html.Div([
+        html.Tr([
+            html.Th('Территориальный орган'),
+            html.Th('Место'),
+            html.Th('Дата'),
+            html.Th('Развитие событий')
+        ], className='table-line-one'),
+        html.Tr([
+            html.Td('Южно-Сибирское межрегиональное управление'),
+            html.Td('Норильская ТЭЦ-3'),
+            html.Td('29 мая 2020 г'),
+            html.Td(children=[
+                html.A(
+                html.Img(
+                    id='alarm-logo',
+                    src='data:image/jpg;base64,{}'.format(
+                        base64.b64encode(open('assets/alarm.png', 'rb').read()).decode())
+                ), href='/norilsk')
+            ]
+            )], className='table-line-two'),
+        html.Tr([
+            html.Td('!ДЕМО! Межрегиональное управление по   г. Москве и Колужской области'),
+            html.Td('!ДЕМО! лесной пожар'),
+            html.Td('!ДЕМО! 10 мая 2020 г'),
+            html.Td(children=[
+                html.Img(
+                    id='alarm-logo',
+                    src='data:image/jpg;base64,{}'.format(
+                        base64.b64encode(open('assets/fire.PNG', 'rb').read()).decode())
+                )
+            ], className = 'table-line-three'),
+        ]),
+        html.Tr([
+            html.Td('!ДЕМО! Межрегиональное управление по Ростовской области и Республике Калмыкия'),
+            html.Td('!ДЕМО! паводок'),
+            html.Td('!ДЕМО! 1 апреля 2020 г'),
+            html.Td(children=[
+                html.Img(
+                        id='alarm-logo',
+                        src='data:image/jpg;base64,{}'.format(
+                            base64.b64encode(open('assets/water.jpg', 'rb').read()).decode())
+                    )
+                ], className='table-line-four')
+        ])
+    ])
+
+    return table
+
 start = html.Div([
     generate_frontpage(),
-    get_buttons()
+    table_link()
 ])
 
 if __name__ == '__main__':
