a4d962273192055bb2f03c6bfc7de89c6b39db0c
IlyaVoltov <39305458+IlyaVoltov@users.noreply.github.com>
2020-06-20 13:40:15 +0300

    Add files via upload

--- 1page.py
+++ 1page.py
@@ -5,14 +5,36 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from norilsk import norilsk
+from norilsk2 import norilsk2
+import base64
 
 app = dash.Dash(__name__)
 server = app.server
+app.css.config.serve_locally = True
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=True),
     html.Div(id='page-content')
 ])
+
+def generate_frontpage(title):
+    frontpage = html.Div(id='las-header', children=[
+        html.A(
+            html.Img(
+                id='las-logo',
+                src='data:image/png;base64,{}'.format(
+                    base64.b64encode(
+                        open('assets/rosprirodnadzor.png', 'rb').read()
+                    ).decode()
+                )), href='https://rpn.gov.ru/'),
+        html.Div(
+            id='las-header-text',
+            children=[
+                html.H1(title)]
+                )
+        ])
+
+    return  frontpage
 
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
@@ -21,39 +43,64 @@ def display_page(pathname):
         return start
     elif pathname == '/norilsk':
         return norilsk
+    elif pathname == '/norilsk2':
+        return norilsk2
     else:
         return start
-    '''
-    elif pathname == '/volhov':
-        return volhov
-    elif pathname == '/northwest':
-        return northwest
-    '''
 
-def get_menu():
-
-    menu = html.Div([
-            dcc.Link('Норильская ТЭЦ-3   ', href='/norilsk', id='link-1', className='tab first'),
-            dcc.Link('Волховская ГЭС  ', href='/volhov', id='link-2', className='tab'),
-            dcc.Link('Севро-Западная ТЭЦ  ', href='/northwest', id='link-3', className='tab'),
-        ], className='screen-top')
-
-    return menu
-
-def header():
-    return html.Div([
-        html.Div([
-            html.H2("Система мониторига аварийных ситуаций")
+def table_link():
+    table = html.Div([
+        html.Tr([
+            html.Th('Территориальный орган'),
+            html.Th('Место'),
+            html.Th('Дата'),
+            html.Th('Развитие событий')
+        ], className='table-line-one'),
+        html.Tr([
+            html.Td('Южно-Сибирское межрегиональное управление'),
+            html.Td('Норильская ТЭЦ-3'),
+            html.Td('29 мая 2020 г'),
+            html.Td(children=[
+                html.A(
+                html.Img(
+                    id='alarm-logo',
+                    src='data:image/jpg;base64,{}'.format(
+                        base64.b64encode(open('assets/alarm.png', 'rb').read()).decode())
+                ), href='/norilsk')
+            ]
+            )], className='table-line-two'),
+        html.Tr([
+            html.Td('!ДЕМО! Межрегиональное управление по   г. Москве и Калужской области'),
+            html.Td('!ДЕМО! лесной пожар'),
+            html.Td('!ДЕМО! 10 мая 2020 г'),
+            html.Td(children=[
+                html.Img(
+                    id='alarm-logo',
+                    src='data:image/jpg;base64,{}'.format(
+                        base64.b64encode(open('assets/fire.PNG', 'rb').read()).decode())
+                )
+            ], className = 'table-line-three'),
         ]),
-    ], className='header')
-
-start = html.Div([
-        header(),
-        get_menu(),
-        html.Div([
-            html.H1("That's a placeholder")
+        html.Tr([
+            html.Td('!ДЕМО! Межрегиональное управление по Ростовской области и Республике Калмыкия'),
+            html.Td('!ДЕМО! паводок'),
+            html.Td('!ДЕМО! 1 апреля 2020 г'),
+            html.Td(children=[
+                html.Img(
+                        id='alarm-logo',
+                        src='data:image/jpg;base64,{}'.format(
+                            base64.b64encode(open('assets/water.jpg', 'rb').read()).decode())
+                    )
+                ], className='table-line-four')
         ])
     ])
+
+    return table
+
+start = html.Div([
+    generate_frontpage('Авария'),
+    table_link()
+])
 
 if __name__ == '__main__':
     app.run_server(debug = True)
