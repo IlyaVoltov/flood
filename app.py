c07ca7d47c767c6211da0e13937734d9ee1650ea
Detlaf <katya.gleh@yandex.ru>
2020-06-25 14:36:15 +0300

    removed run_server from app

--- app.py
+++ app.py
@@ -10,5 +10,3 @@ from dash.dependencies import Input, Output
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
 
-if __name__ == '__main__':
-    app.run_server(host='0.0.0.0')
