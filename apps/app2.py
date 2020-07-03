5e8ee4f714fc870db9485a1d7ef30d8f21b90d03
IlyaVoltov <39305458+IlyaVoltov@users.noreply.github.com>
2020-07-03 10:29:47 +0300

    Update app2.py

--- apps/app2.py
+++ apps/app2.py
@@ -260,10 +260,6 @@ tab_style = {
     'color' : '#696969'
 }
 
-tab_style:hover = {
-     'height'  : '46px',
-}
-
 tab_selected_style = {
     'borderTop': '1px solid #d6d6d6',
     'borderBottom': '1px solid #d6d6d6',
